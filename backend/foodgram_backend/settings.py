095d51eb6ce525d3dad460402ef6c016acfa7dd4
Michael-Burka <contact@michaelburka.com>
2023-10-21 15:11:33 +0400

    Refactor settings: add PostgreSQL db name and role

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -53,8 +53,8 @@ WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
+        'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
